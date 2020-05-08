8b62f779cb9e813c59b20ed135642143d8d4fb72
Thanos Papathanasiou <thanospapathanasiou@hotmail.com>
2020-05-08 15:09:41 +0100

    build your own lisp, chapter 7

--- parsing.c
+++ parsing.c
@@ -28,6 +28,33 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
+long eval_op(long x, char* op, long y){
+    if (strcmp(op, "+") == 0 ) { return x + y; }
+    if (strcmp(op, "-") == 0 ) { return x - y; }
+    if (strcmp(op, "*") == 0 ) { return x * y; }
+    if (strcmp(op, "/") == 0 ) { return x / y; }
+    return 0;
+}
+
+long eval(mpc_ast_t* t){
+    if (strstr(t->tag, "number")){
+        return atoi(t->contents);
+    }
+
+    char* op = t->children[1]->contents;
+    long x = eval(t->children[2]);
+
+    int i = 3;
+
+    while (strstr(t->children[i]->tag, "expr")){
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
+
 int main(int argc, char** argv){
 
     mpc_parser_t* Number    = mpc_new("number");
@@ -44,18 +71,18 @@ int main(int argc, char** argv){
         ",
         Number, Operator, Expr, Lispy);
 
-    puts("λispy Version 0.0.2");
+    puts("λispy Version 0.0.3");
     puts("Press Ctrl+c to exit\n");
 
-    while(true){
+    while (true){
         char* input = readline("λ> ");
-
         add_history(input);
 
         mpc_result_t r;
-        
         if(mpc_parse("<stdin>", input, Lispy, &r)){
-            mpc_ast_print(r.output);
+            long result = eval(r.output);
+            printf("%li\n", result);
+            //mpc_ast_print(r.output);
             mpc_ast_delete(r.output);
         } else {
             mpc_err_print(r.error);
